4204efd2ad284899b3b04d02e45590d351c3a307
mariecreel <nick.creel@protonmail.com>
2022-05-07 13:45:29 -0400

    configure eslint, run eslint --fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 // must pass class name and attach to html
 // for styled component styles to apply
 // see https://styled-components.com/docs/basics#styling-any-component
-function App({ className, children }) {
+/**
+ * @param {string} className - class name for styled components
+ * @return {JSX}
+ */
+function App({className}) {
   return <div className={className}>Hello, world!</div>;
 }
+
+App.propTypes = {
+  className: PropTypes.string.isRequired,
+};
 
 export const StyledApp = styled(App)`
   text-align: center;
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(js)$/, 
-                use: 'babel-loader' 
-            },
-            {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    mode: 'development',
-    plugins: [
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development',
+  plugins: [
 
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
-    ]
-}
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
