7627813fe2589fd6475a08ac25881ea6f990d9f0
mariecreel <nick.creel@protonmail.com>
2022-05-07 12:24:21 -0400

    configure webpack and babel, add styled components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function App() {
+  return <div>Hello, world!</div>;
+}
+
+const StyledApp = styled(App)`
+  box-sizing: border-box;
+  margin: 0 auto;
+  width: 75%;
+  padding: 10px;
+`;
